7073d660ea367b5d9c934bfefe7cbcc67567e38d
Teja Kancherla <tejakancherla.cno@gmail.com>
2016-02-28 23:09:48 +0530

    Added files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -15,24 +15,24 @@ activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
 features <- read.table("UCI HAR Dataset/features.txt")
 
 #To Loan Train and test datasets
-x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
-y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
-subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+traindata_x <- read.table("UCI HAR Dataset/train/X_train.txt")
+traindata_y <- read.table("UCI HAR Dataset/train/y_train.txt")
+traindata_subject <- read.table("UCI HAR Dataset/train/subject_train.txt")
 
-x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
-y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
-subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+testdata_x <- read.table("UCI HAR Dataset/test/X_test.txt")
+testdata_y <- read.table("UCI HAR Dataset/test/y_test.txt")
+testdata_subject <- read.table("UCI HAR Dataset/test/subject_test.txt")
 
 # Step 1:Merging the dataset
 
 # create 'x' data set
-x_data <- rbind(x_train, x_test)
+merge_data_x <- rbind(traindata_x, testdata_x)
 
 # create 'y' data set
-y_data <- rbind(y_train, y_test)
+merge_data_y <- rbind(traindata_y, testdata_y)
 
 # create 'subject' data set
-subject_data <- rbind(subject_train, subject_test)
+merge_data_subject <- rbind(traindata_subject, testdata_subject )
 
 #Extracts only the measurements on the mean and standard deviation for 
 #each measurement.
@@ -41,22 +41,22 @@ subject_data <- rbind(subject_train, subject_test)
 features_names_mean_sd <- grep(".*mean.*|.*std.*", features[, 2])
 
 # subset the desired columns
-x_data <- x_data[, features_names_mean_sd]
+merge_data_x <- merge_data_x[, features_names_mean_sd]
 
 # correct the column names
-names(x_data) <- features[features_names_mean_sd, 2]
+names(merge_data_x) <- features[features_names_mean_sd, 2]
 
 ##Step 3: Assigning appropriate names to activities
-names(y_data) <- "activities"
-y_data[,1] <- activityLabels[y_data[,1],2]
+names(merge_data_y) <- "activities"
+merge_data_y[,1] <- activityLabels[merge_data_y[,1],2]
 # Step 4
 # Appropriately label the data set with descriptive variable names
 ###############################################################################
 # correct column name
-names(subject_data) <- "subject"
+names(merge_data_subject) <- "subject"
 
 # bind all the data in a single data set
-consolidated_data <- cbind(subject_data,y_data,x_data)
+consolidated_data <- cbind(merge_data_subject,merge_data_y,merge_data_x)
 
 # Step 5 
 # To prepare data set with average of all measurements(mean & std) for each activty & subject
